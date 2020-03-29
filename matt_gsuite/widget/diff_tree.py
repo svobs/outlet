7619ba3d59eb957f3d96c0d55a0ae1aa34a94366
Matt Svoboda <matt.svoboda@gmail.com>
2020-03-29 13:29:37 -0700

    Icons work!

--- matt_gsuite/widget/diff_tree.py
+++ matt_gsuite/widget/diff_tree.py
@@ -2,7 +2,7 @@ import os
 from datetime import datetime
 import humanfriendly
 from fmeta.fmeta import FMetaSet
-from gi.repository import GLib, Gtk
+from gi.repository import GLib, Gtk, Gdk, GdkPixbuf
 from enum import Enum, auto
 from treelib import Node, Tree
 
@@ -19,13 +19,16 @@ class ChangeSet:
 
 class Category(Enum):
     ADDED = auto()
+    UPDATED = auto()
+    DELETED = auto()
     UNEXPECTED = auto()
 
 
-cat_names = {Category.ADDED: 'Added', Category.UNEXPECTED: 'Unexpected'}
+cat_names = {Category.ADDED: 'Added', Category.DELETED: 'Deleted', Category.UPDATED: 'Updated', Category.UNEXPECTED: 'Unexpected'}
 
 
 class DiffTree:
+
     def __init__(self):
         # The source files
         self.fmeta_set = FMetaSet()
@@ -39,14 +42,17 @@ class DiffTree:
 
 
         if self.use_dir_tree:
-            self.model = Gtk.TreeStore(str, str, str, str)
-        else:
             self.model = Gtk.TreeStore(str, str, str, str, str)
+        else:
+            self.model = Gtk.TreeStore(str, str, str, str, str, str)
         self.category_unexpected = None
         self.category_added = None
         self.category_removed = None
 
     #def build_gtk_tree(self):
+
+        #self.icon = Gtk.IconTheme.get_default().load_icon("folder", 22, 0)
+        self.icon = GdkPixbuf.Pixbuf.new_from_file(DiffTree.get_resource_path("../../resources/fslint_icon.png"))
 
         # The UI tree widget
         self.tree = Gtk.TreeView(model=self.model) #TODO: detach from model while populating
@@ -54,16 +60,34 @@ class DiffTree:
         self.tree.set_show_expanders(True)
         self.tree.set_property('enable_grid_lines', True)
         self.tree.set_property('enable_tree_lines', True)
+        self.tree.set_fixed_height_mode(True)
+        # Allow click+drag to select multiple items.
+        # May want to disable if using drag+drop
+        self.tree.set_rubber_banding(True)
+
+        # 0 ICON
+        col_num = 1
+        px_renderer = Gtk.CellRendererPixbuf()
+        px_column = Gtk.TreeViewColumn('')
+        px_column.set_sizing(Gtk.TreeViewColumnSizing.FIXED)
+        px_column.pack_start(px_renderer, False)
+        str_renderer = Gtk.CellRendererText()
+        px_column.pack_start(str_renderer, False)
+        # set data connector function/method
+        px_column.set_cell_data_func(px_renderer, self.get_tree_cell_pixbuf)
+        px_column.set_cell_data_func(str_renderer, self.get_tree_cell_text)
+        self.tree.append_column(px_column)
 
         # 1 NAME
-        col_num = 1
+        col_num += 1
         renderer = Gtk.CellRendererText()
         # Set desired number of chars width
         renderer.set_property('width-chars', 15)
-        column = Gtk.TreeViewColumn("Name", renderer, text=col_num)
+        column = Gtk.TreeViewColumn(title="Name", cell_renderer=renderer, text=col_num)
+       # column.add_attribute(pixbuf_renderer, "pixbuf", 0)
         column.set_sort_column_id(col_num)
 
-        column.set_sizing(Gtk.TreeViewColumnSizing.AUTOSIZE)
+        column.set_sizing(Gtk.TreeViewColumnSizing.FIXED)
         #  column.set_fixed_width(50)
         column.set_min_width(50)
         # column.set_max_width(300)
@@ -80,7 +104,7 @@ class DiffTree:
             column = Gtk.TreeViewColumn("Directory", renderer, text=col_num)
             column.set_sort_column_id(col_num)
 
-            column.set_sizing(Gtk.TreeViewColumnSizing.AUTOSIZE)
+            column.set_sizing(Gtk.TreeViewColumnSizing.FIXED)
             column.set_min_width(50)
             # column.set_max_width(300)
             column.set_expand(True)
@@ -95,7 +119,7 @@ class DiffTree:
         column = Gtk.TreeViewColumn("Size", renderer, text=col_num)
         column.set_sort_column_id(col_num)
 
-        column.set_sizing(Gtk.TreeViewColumnSizing.AUTOSIZE)
+        column.set_sizing(Gtk.TreeViewColumnSizing.FIXED)
         #  column.set_fixed_width(50)
         column.set_min_width(50)
         # column.set_max_width(300)
@@ -126,7 +150,7 @@ class DiffTree:
         column = Gtk.TreeViewColumn("Modification Date", renderer, text=col_num)
         column.set_sort_column_id(col_num)
 
-        column.set_sizing(Gtk.TreeViewColumnSizing.AUTOSIZE)
+        column.set_sizing(Gtk.TreeViewColumnSizing.FIXED)
         #column.set_fixed_width(50)
         column.set_min_width(50)
         # column.set_max_width(300)
@@ -144,7 +168,16 @@ class DiffTree:
         select.set_mode(Gtk.SelectionMode.MULTIPLE)
         select.connect("changed", on_tree_selection_changed)
 
-    # Builds a tree out of the flat change set.
+    def get_tree_cell_text(self, col, cell, model, iter, user_data):
+        cell.set_property('text', model.get_value(iter, 1))
+
+    def get_tree_cell_pixbuf(self, col, cell, model, iter, user_data):
+       # cell.set_property('pixbuf', GdkPixbuf.Pixbuf.new_from_file(model.get_value(iter, 0)))
+        cell.set_property('pixbuf', self.icon)
+
+
+
+# Builds a tree out of the flat change set.
     @staticmethod
     def build_category_change_tree(change_set, cat_name):
         # The change set in tree form
@@ -180,19 +213,28 @@ class DiffTree:
 
     def append_dir(self, tree_iter, dir_name):
         if self.use_dir_tree:
-            return self.model.append(tree_iter, [None, dir_name, None, None])
+            return self.model.append(tree_iter, ['icon.png', None, dir_name, None, None])
         else:
-            return self.model.append(tree_iter, [None, dir_name, None, None, None])
+            return self.model.append(tree_iter, ['icon.png', None, dir_name, None, None, None])
+
+    @staticmethod
+    def get_resource_path(rel_path):
+        # Get absolute path from the given relative path (relative to this file's location)
+        dir_of_py_file = os.path.dirname(__file__)
+        rel_path_to_resource = os.path.join(dir_of_py_file, rel_path)
+        abs_path_to_resource = os.path.abspath(rel_path_to_resource)
+        return abs_path_to_resource
 
     def append_fmeta(self, tree_iter, file_name, fmeta):
         num_bytes_str = humanfriendly.format_size(fmeta.length)
         modify_datetime = datetime.fromtimestamp(fmeta.modify_ts)
         modify_time = modify_datetime.strftime("%Y-%m-%d %H:%M:%S")
+
         if self.use_dir_tree:
-            return self.model.append(tree_iter, [fmeta.signature, file_name, num_bytes_str, modify_time])
+            return self.model.append(tree_iter, ['icon.png', fmeta.signature, file_name, num_bytes_str, modify_time])
         else:
             directory, name = os.path.split(fmeta.file_path)
-            return self.model.append(tree_iter, [fmeta.signature, file_name, directory, num_bytes_str, modify_time])
+            return self.model.append(tree_iter, ['icon.png', fmeta.signature, file_name, directory, num_bytes_str, modify_time])
 
     def _populate_category(self, cat_name, change_set):
         change_tree = DiffTree.build_category_change_tree(change_set, cat_name)
@@ -213,7 +255,6 @@ class DiffTree:
                 root = change_tree.get_node('')
                 append_recursively(None, root)
 
-            # TODO: other categories
             self.tree.expand_all()
 
         GLib.idle_add(do_on_ui_thread)
