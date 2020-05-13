3aeb57ccd536de9bd0c41681a8fd9e1bef934bb1
Matt Svoboda <matt.svoboda@gmail.com>
2020-05-13 03:45:46 -0700

    Finally fix progress bar animation

--- ultrasync/ui/comp/progress_bar.py
+++ ultrasync/ui/comp/progress_bar.py
@@ -1,28 +1,36 @@
+import threading
+import time
+
 import ui.actions as actions
 import logging
 
 import gi
+
+from constants import PROGRESS_BAR_MAX_MSG_LENGTH, PROGRESS_BAR_PULSE_STEP, PROGRESS_BAR_SLEEP_TIME_SEC
+
 gi.require_version("Gtk", "3.0")
 from gi.repository import GLib, Gtk
 
 logger = logging.getLogger(__name__)
 
-PULSE_STEP = 0.0001
-MAX_MSG_LENGTH = 80
-
 
 class ProgressBar:
     def __init__(self, config, sender_list):
-        self.update_interval_ms = float(config.get('display.max_refresh_interval_sec', 0.001)) * 1000
+        self._update_interval_ms = float(config.get('display.max_refresh_interval_sec', 0.001)) * 1000
         self.progressbar = Gtk.ProgressBar()
-        self.timeout_id = None
-        self.progress = 0
-        self.total = 0
-        self.done = False
-        self.progressbar.set_pulse_step(PULSE_STEP)
+
+        # state:
+        self._progress = 0
+        self._total = 0
+        self._indeterminate = False
+
+        # lifecycle:
+        self._stop_requested = False
+        self._stop_request_count = 0
+        self._start_request_count = 0
+
+        self.progressbar.set_pulse_step(PROGRESS_BAR_PULSE_STEP)
         self.progressbar.hide()
-        self.indeterminate = False
-        self.transactions = {}
         """For keeping track of start/stop requests when they can occur in any order"""
 
         for sender in sender_list:
@@ -33,87 +41,76 @@ class ProgressBar:
             actions.connect(signal=actions.STOP_PROGRESS, handler=self.on_stop_progress, sender=sender)
             actions.connect(signal=actions.SET_PROGRESS_TEXT, handler=self.on_set_progress_text, sender=sender)
 
-    def _start_animaion(self):
+    def _start_animaion(self, total=None):
+        self._start_request_count += 1
+        logger.debug(f'StartProgress(): Starts: {self._start_request_count}, Stops: {self._stop_request_count}')
+        if self._start_request_count != self._stop_request_count + 1:
+            logger.debug('Discarding start request because we have not received enough stop requests yet')
+            return
+
+        if total:
+            self._total = total
+            self._progress = 0
+
         def start_animation():
             self.progressbar.show()
-            self.timeout_id = GLib.timeout_add(self.update_interval_ms, self.on_timeout)
-            logger.debug(f'Started a progress bar animation with timeout_id: {self.timeout_id}')
+            self.progressbar.set_show_text(False)
 
         GLib.idle_add(start_animation)
 
+        thread = threading.Thread(target=self.run_thread, daemon=True)
+        thread.start()
+
     def on_start_progress_indeterminate(self, tx_id, sender):
-        if self.transactions.get(tx_id, None):
-            logger.debug(f'Ignoring progress_made; already completed: {tx_id}')
-            return
-
-        self.done = False
-        self.indeterminate = True
-
-        if self.timeout_id:
-            logger.debug(f'Starting a ProgressBar which has already been started: {self.timeout_id}')
-        else:
-            self._start_animaion()
+        self._indeterminate = True
+        self._start_animaion()
 
     def on_start_progress(self, sender, tx_id, total):
-        if self.transactions.get(tx_id, None):
-            logger.debug(f'Ignoring progress_made; already completed: {tx_id}')
-            if tx_id in self.transactions: del self.transactions[tx_id]
-            return
-
-        self.done = False
-        self.progress = 0
-        self.total = float(total)
-        self.indeterminate = False
-
-        if self.timeout_id:
-            logger.debug(f'Starting a ProgressBar which has already been started: {self.timeout_id}')
-        else:
-            self._start_animaion()
+        self._indeterminate = False
+        self._start_animaion(total=total)
 
     def on_progress_made(self, sender, tx_id, progress):
-        if self.transactions.get(tx_id, None):
-            logger.debug(f'Ignoring progress_made; already completed: {tx_id}')
-            return
-        self.progress += progress
+        self._progress += progress
 
     def on_stop_progress(self, tx_id, sender):
-        if self.transactions.get(tx_id, None):
-            if tx_id in self.transactions: del self.transactions[tx_id]
-        else:
-            self.transactions[tx_id] = 'done'
-        self.done = True
-        self.timeout_id = None
+        self._stop_request_count += 1
+        logger.debug(f'StopProress(): Starts: {self._start_request_count}, Stops: {self._stop_request_count}')
+        if self._start_request_count > self._stop_request_count:
+            logger.debug('Discarding stop request because we have not received enough start requests yet')
+            return
         logger.debug(f'Stopped progress animation')
+        self._stop_requested = True
 
     def on_set_progress_text(self, sender, msg, tx_id=None):
-        if len(msg) > MAX_MSG_LENGTH:
-            msg = msg[:(MAX_MSG_LENGTH-3)] + '...'
+        if len(msg) > PROGRESS_BAR_MAX_MSG_LENGTH:
+            msg = msg[:(PROGRESS_BAR_MAX_MSG_LENGTH-3)] + '...'
 
         def set_text(m):
             self.progressbar.set_show_text(True)
             self.progressbar.set_text(m)
         GLib.idle_add(set_text, msg)
 
-    def on_timeout(self):
+    def run_thread(self):
         """
         Update value on the progress bar
         """
-        if self.done:
-            self.progressbar.set_show_text(False)
-            # Stop looping
-            self.progressbar.hide()
-            logger.debug('Hiding progress animation because it is done')
-            return False
+        def throb():
+            if self._indeterminate:
+                self.progressbar.pulse()
+            else:
+                new_value = self._progress / self._total
+                if new_value > 1:
+                    new_value = 1
 
-        if self.indeterminate:
-            self.progressbar.pulse()
-        else:
-            new_value = self.progress / self.total
-            if new_value > 1:
-                new_value = 1
+                self.progressbar.set_fraction(new_value)
 
-            self.progressbar.set_fraction(new_value)
+        logger.debug('Starting')
+        while not self._stop_requested:
+            time.sleep(PROGRESS_BAR_SLEEP_TIME_SEC)
+            GLib.idle_add(throb)
 
-        # As this is a timeout function, return True so that it continues to get called
-        return True
+        self._stop_requested = False
+        # Stop looping
+        self.progressbar.hide()
+        logger.debug('Stopping')
 
