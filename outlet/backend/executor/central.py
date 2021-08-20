4a98b007f91f96ef1bcd3c1a412a8492bad6bef2
Matt Svoboda <matt.svoboda@gmail.com>
2021-08-20 15:16:18 -0700

    Fix critical bug in executor task ordering: child tasks and next_tasks now have priority over the regular queue

--- outlet/backend/executor/central.py
+++ outlet/backend/executor/central.py
@@ -1,10 +1,11 @@
 import logging
 import threading
+from collections import deque
 from concurrent.futures import Future
 from enum import IntEnum
 from functools import partial
 from queue import Empty, Queue
-from typing import Dict, Optional, Set, Tuple
+from typing import Deque, Dict, Optional, Set, Tuple
 from uuid import UUID
 
 from pydispatch import dispatcher
@@ -58,13 +59,15 @@ class CentralExecutor(HasLifecycle):
         self._command_executor = CommandExecutor(self.backend)
         self._global_actions = GlobalActions(self.backend)
         self._be_task_runner = TaskRunner()
-        self._running_task_dict: Dict[UUID, Task] = {}
         self.enable_op_execution = backend.get_config('executor.enable_op_execution')
         self._lock = threading.Lock()
         self._running_task_cv = threading.Condition(self._lock)
 
+        self._PRIORITIES = [ExecPriority.P0_USER_LOAD, ExecPriority.P1_BACKGROUND_CACHE_LOAD, ExecPriority.P2_DOWNLOAD, ExecPriority.P3_LIVE_UPDATE,
+                            ExecPriority.P4_SIGNATURE_CALC, ExecPriority.P5_USER_OP_EXECUTION]
+
         # -- QUEUES --
-        self._exec_queue_dict: Dict[ExecPriority, Queue[Task]] = {
+        self._submitted_task_queue_dict: Dict[ExecPriority, Queue[Task]] = {
 
             ExecPriority.P0_USER_LOAD: Queue[Task](),
 
@@ -79,8 +82,24 @@ class CentralExecutor(HasLifecycle):
             ExecPriority.P5_USER_OP_EXECUTION: Queue[Task](),
         }
 
-        self._parent_child_task_dict: Dict[UUID, Set[UUID]] = {}
-        self._waiting_parent_task_dict: Dict[UUID, Task] = {}
+        self._next_task_queue_dict: Dict[ExecPriority, Queue[Task]] = {
+
+            ExecPriority.P0_USER_LOAD: Queue[Task](),
+
+            ExecPriority.P1_BACKGROUND_CACHE_LOAD: Queue[Task](),
+
+            ExecPriority.P2_DOWNLOAD: Queue[Task](),
+
+            ExecPriority.P3_LIVE_UPDATE: Queue[Task](),
+
+            ExecPriority.P4_SIGNATURE_CALC: Queue[Task](),
+
+            ExecPriority.P5_USER_OP_EXECUTION: Queue[Task](),
+        }
+
+        self._running_task_dict: Dict[UUID, Task] = {}
+        self._parent_child_task_dict: Dict[UUID, Deque[UUID]] = {}
+        self._dependent_task_dict: Dict[UUID, Task] = {}
 
         self._central_exec_thread: threading.Thread = threading.Thread(target=self._run_central_exec_thread,
                                                                        name=CENTRAL_EXEC_THREAD_NAME, daemon=True)
@@ -114,13 +133,13 @@ class CentralExecutor(HasLifecycle):
     def get_engine_summary_state(self) -> EngineSummaryState:
         with self._lock:
             # FIXME: need to revisit these categories
-            if self._exec_queue_dict[ExecPriority.P1_BACKGROUND_CACHE_LOAD].qsize() > 0 \
-                    or self._exec_queue_dict[ExecPriority.P2_DOWNLOAD].qsize() > 0:
+            if self._submitted_task_queue_dict[ExecPriority.P1_BACKGROUND_CACHE_LOAD].qsize() > 0 \
+                    or self._submitted_task_queue_dict[ExecPriority.P2_DOWNLOAD].qsize() > 0:
                 # still getting up to speed on the BE
                 return EngineSummaryState.RED
 
-            total_enqueued = self._exec_queue_dict[ExecPriority.P0_USER_LOAD].qsize() + \
-                             self._exec_queue_dict[ExecPriority.P4_SIGNATURE_CALC].qsize()
+            total_enqueued = self._submitted_task_queue_dict[ExecPriority.P0_USER_LOAD].qsize() + \
+                             self._submitted_task_queue_dict[ExecPriority.P4_SIGNATURE_CALC].qsize()
 
             if total_enqueued > 0:
                 return EngineSummaryState.YELLOW
@@ -149,14 +168,14 @@ class CentralExecutor(HasLifecycle):
                         task = self._get_next_task_to_run()
                         if task:
                             if TRACE_ENABLED:
-                                logger.debug(f'[{CENTRAL_EXEC_THREAD_NAME}] Popped task with priority {task.priority.name}: '
+                                logger.debug(f'[{CENTRAL_EXEC_THREAD_NAME}] Got task with priority {task.priority.name}: '
                                              f'"{task.task_func.__name__}" uuid={task.task_uuid}')
                             self._running_task_dict[task.task_uuid] = task
                         elif TRACE_ENABLED:
                             logger.debug(f'[{CENTRAL_EXEC_THREAD_NAME}] No queued tasks to run (currently running: {len(self._running_task_dict)})')
                     else:
                         logger.debug(f'[{CENTRAL_EXEC_THREAD_NAME}] Running task queue is at max capacity ({TASK_RUNNER_MAX_WORKERS}): '
-                                     f'{self._print_running_task_dict()}')
+                                     f'{self._print_running_task_dict()}. WaitingParents={self._dependent_task_dict.values()}')
 
                 # Do this outside the CV:
                 if task:
@@ -180,31 +199,20 @@ class CentralExecutor(HasLifecycle):
 
     def _get_from_queue(self, priority: ExecPriority) -> Optional[Task]:
         try:
-            task = self._exec_queue_dict[priority].get_nowait()
+            task = self._next_task_queue_dict[priority].get_nowait()
             return task
         except Empty:
-            return None
+            try:
+                task = self._submitted_task_queue_dict[priority].get_nowait()
+                return task
+            except Empty:
+                return None
 
     def _get_next_task_to_run(self) -> Optional[Task]:
-        task = self._get_from_queue(ExecPriority.P0_USER_LOAD)
-        if task:
-            return task
-
-        task = self._get_from_queue(ExecPriority.P1_BACKGROUND_CACHE_LOAD)
-        if task:
-            return task
-
-        task = self._get_from_queue(ExecPriority.P2_DOWNLOAD)
-        if task:
-            return task
-
-        task = self._get_from_queue(ExecPriority.P3_LIVE_UPDATE)
-        if task:
-            return task
-
-        task = self._get_from_queue(ExecPriority.P4_SIGNATURE_CALC)
-        if task:
-            return task
+        for priority in self._PRIORITIES:
+            task = self._get_from_queue(priority)
+            if task:
+                return task
 
         if self.enable_op_execution:
 
@@ -227,14 +235,14 @@ class CentralExecutor(HasLifecycle):
         return None
 
     def _on_task_done(self, done_task: Task, future: Future):
+        if SUPER_DEBUG_ENABLED:
+            # whether it succeeded, failed or was cancelled is of little difference from our standpoint; just make a note
+            if future.cancelled():
+                logger.debug(f'Task cancelled: func="{done_task.task_func.__name__}" uuid={done_task.task_uuid}, priority={done_task.priority.name}')
+            else:
+                logger.debug(f'Task done: "{done_task.task_func.__name__}" uuid={done_task.task_uuid}, priority={done_task.priority.name}')
 
         with self._running_task_cv:
-            if SUPER_DEBUG_ENABLED:
-                # whether it succeeded, failed or was cancelled is of little difference from our standpoint; just make a note
-                if future.cancelled():
-                    logger.debug(f'Task cancelled: func="{done_task.task_func.__name__}" uuid={done_task.task_uuid}, priority={done_task.priority.name}')
-                else:
-                    logger.debug(f'Task done: "{done_task.task_func.__name__}" uuid={done_task.task_uuid}, priority={done_task.priority.name}')
 
             # Removing based on object identity should work for now, since we are in the same process:
             # Note: this is O(n). Best not to let the deque get too large
@@ -242,24 +250,24 @@ class CentralExecutor(HasLifecycle):
             logger.debug(f'RunningTaskDict now has {len(self._running_task_dict)} tasks')
 
             # Did this done_task spawn child tasks which need to be waited for?
-            child_set_of_done_task = self._parent_child_task_dict.get(done_task.task_uuid, None)
-            if child_set_of_done_task:
-                logger.debug(f'Task {done_task.task_uuid} has {len(child_set_of_done_task)} children to run '
-                             f'({", ".join([ str(u) for u in child_set_of_done_task])}): will run its next_task after they are done')
-                # add to _waiting_parent_task_dict and do not remove it until ready to run its next_task
-                self._waiting_parent_task_dict[done_task.task_uuid] = done_task
+            child_deque_of_done_task: Deque[UUID] = self._parent_child_task_dict.get(done_task.task_uuid, None)
+            if child_deque_of_done_task:
+                logger.debug(f'Task {done_task.task_uuid} has {len(child_deque_of_done_task)} children to run '
+                             f'({", ".join([ str(u) for u in child_deque_of_done_task])}): will run its next_task after they are done')
+                # add to _dependent_task_dict and do not remove it until ready to run its next_task
+                self._dependent_task_dict[done_task.task_uuid] = done_task
                 self._running_task_cv.notify_all()
                 return
 
             next_task = self._find_next_task(done_task)
-            if not next_task:
-                # wake up main thread, and allow it to run next task in queue
-                self._running_task_cv.notify_all()
-                return
+            if next_task:
+                # special queue
+                logger.debug(f'Enqueuing next task {next_task.task_uuid} with parent={next_task.parent_task_uuid} '
+                             f'(for completed task {done_task.task_uuid})')
+                self._next_task_queue_dict[next_task.priority].put_nowait(next_task)
 
-        logger.debug(f'Submitting next task {next_task.task_uuid} with parent={next_task.parent_task_uuid} '
-                     f'(for completed task {done_task.task_uuid})')
-        self.submit_async_task(next_task)
+            # wake up main thread, and allow it to run next task in queue
+            self._running_task_cv.notify_all()
 
         # TODO: DELETE task if completely done, to prevent memory leaks due to circular references
 
@@ -272,18 +280,18 @@ class CentralExecutor(HasLifecycle):
         # Was this task a child of some other parent task?
         if done_task.parent_task_uuid:
             logger.debug(f'Task {done_task.task_uuid} ({done_task.task_func}) was a child of parent task {done_task.parent_task_uuid}')
-            parent_child_set = self._parent_child_task_dict.get(done_task.parent_task_uuid, None)
-            if parent_child_set is None:
+            child_deque: Deque[UUID] = self._parent_child_task_dict.get(done_task.parent_task_uuid, None)
+            if child_deque is None:
                 raise RuntimeError(f'Serious internal error: state of parent & child tasks is inconsistent! '
                                    f'ParentChildDict={self._parent_child_task_dict} ChildTask={done_task}')
             # update the parent's child set; when it is empty, it is officially complete
-            parent_child_set.remove(done_task.task_uuid)
+            child_deque.remove(done_task.task_uuid)
 
             if next_task:
                 # If task was a child of parent, make its next_task also a child of parent and run that before parent's next_task
-                parent_child_set.add(next_task.task_uuid)
+                child_deque.appendleft(next_task.task_uuid)
                 # fail if not present
-                existing_parent_task = self._waiting_parent_task_dict[done_task.parent_task_uuid]
+                existing_parent_task = self._dependent_task_dict[done_task.parent_task_uuid]
                 assert existing_parent_task.task_uuid == done_task.parent_task_uuid, \
                     f'Should be equal: {existing_parent_task.task_uuid} and {done_task.parent_task_uuid}'
 
@@ -294,28 +302,38 @@ class CentralExecutor(HasLifecycle):
                     # Programmer error when creating tasks
                     raise RuntimeError(f'Inconsistent state! Task ({done_task}) has a different parent than its next_task ({next_task})')
 
+                self._dependent_task_dict[next_task.task_uuid] = next_task
+
                 return next_task
 
-            if not parent_child_set:
+            if not child_deque:
                 # No next_task and no children left in parent task: run parent's next_task
 
                 # clean up data structure: children are all completed:
                 self._parent_child_task_dict.pop(done_task.parent_task_uuid)
 
-                done_parent_task = self._waiting_parent_task_dict.pop(done_task.parent_task_uuid, None)
+                done_parent_task = self._dependent_task_dict.pop(done_task.parent_task_uuid, None)
                 if not done_parent_task:
                     raise RuntimeError(f'Serious internal error: failed to find expected parent task '
                                        f'({done_task.parent_task_uuid}) in waiting_parent_dict!)')
-                logger.debug(f'Parent task {done_parent_task.task_uuid} has no children left; recursing')
+                logger.debug(f'Parent task {done_parent_task.task_uuid} ({done_parent_task.task_func}) has no children left; recursing')
 
                 # Go up next level in the tree and repeat logic:
                 return self._find_next_task(done_parent_task)
 
             else:
-                logger.debug(f'Parent task {done_task.parent_task_uuid} still has {len(parent_child_set)} children left to run')
-                return None
+                logger.debug(f'Parent task {done_task.parent_task_uuid} still has {len(child_deque)} children left to run: returning first child')
+                child_uuid = child_deque[0]
+                return self._dependent_task_dict[child_uuid]  # fail if not found
         else:
             return next_task
+
+    def _add_child_to_parent(self, parent_task_uuid, child_task_uuid):
+        child_deque = self._parent_child_task_dict.get(parent_task_uuid, None)
+        if not child_deque:
+            child_deque = deque()
+            self._parent_child_task_dict[parent_task_uuid] = child_deque
+        child_deque.append(child_task_uuid)
 
     def submit_async_task(self, task: Task):
         """API: enqueue task to be executed, with given priority."""
@@ -330,18 +348,16 @@ class CentralExecutor(HasLifecycle):
                          f'parent: {task.parent_task_uuid})')
 
             if task.parent_task_uuid:
-                if not self._running_task_dict.get(task.parent_task_uuid, None) and not self._waiting_parent_task_dict.get(task.parent_task_uuid):
+                if not self._running_task_dict.get(task.parent_task_uuid, None) and not self._dependent_task_dict.get(task.parent_task_uuid):
                     raise RuntimeError(f'Cannot add task {task.task_uuid}: referenced parent task {task.parent_task_uuid} was not found! '
                                        f'Maybe it and all its children already completed?')
 
-                # Do this *before* putting in queue, in case it gets picked up too quickly
-                child_set = self._parent_child_task_dict.get(task.parent_task_uuid, None)
-                if not child_set:
-                    child_set = set()
-                    self._parent_child_task_dict[task.parent_task_uuid] = child_set
-                child_set.add(task.task_uuid)
-
-            self._exec_queue_dict[priority].put_nowait(task)
+                # Child tasks have priority over the _submitted_task_queue_dict and will be enqueued when appropriate
+                self._add_child_to_parent(parent_task_uuid=task.parent_task_uuid, child_task_uuid=task.task_uuid)
+                self._dependent_task_dict[task.task_uuid] = task
+            else:
+                # No parent: put in the regular queue:
+                self._submitted_task_queue_dict[priority].put_nowait(task)
 
             if len(self._running_task_dict) < TASK_RUNNER_MAX_WORKERS:
                 self._running_task_cv.notify_all()
