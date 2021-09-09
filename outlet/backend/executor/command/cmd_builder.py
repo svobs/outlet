654f80e184c2afc576c1ffefb38361f0a69f6b65
Matt Svoboda <matt.svoboda@gmail.com>
2021-09-09 04:23:22 -0700

    Add acknowledgement (albeit not exactly support) for binary operations on different trees of the same type

--- outlet/backend/executor/command/cmd_builder.py
+++ outlet/backend/executor/command/cmd_builder.py
@@ -1,5 +1,5 @@
 import logging
-from typing import Callable, Dict
+from typing import Callable, Dict, Optional
 
 from backend.executor.command.cmd_impl import CopyFileGDriveCommand, CopyFileLocallyCommand, CreateGDriveFolderCommand, \
     CreatLocalDirCommand, DeleteGDriveNodeCommand, DeleteLocalNodeCommand, \
@@ -9,24 +9,35 @@ from backend.executor.command.cmd_impl import CopyFileGDriveCommand, CopyFileLoc
     UploadToGDriveCommand
 from backend.executor.command.cmd_interface import Command
 from constants import TreeType
+from model.node.node import Node
 from model.user_op import UserOp, UserOpType
 
 logger = logging.getLogger(__name__)
 
 
-def _make_key(tree_type_src, tree_type_dst=None):
+def _make_key(tree_type_src: TreeType, tree_type_dst: Optional[TreeType] = None, is_same_tree: bool = False):
     if tree_type_dst:
-        return f'{tree_type_src}->{tree_type_dst}'
-    return f'{tree_type_src}'
+        return f'{tree_type_src}->{"SAME:" if is_same_tree else ""}{tree_type_dst}'
+    else:
+        return f'{tree_type_src}'
 
 
-# FIXME: distinguish between same device and different devices. For now, we will assume these are all same-device
+def _make_key_from_node(node_src: Node, node_dst: Optional[Node] = None):
+    if node_dst:
+        is_same_tree = node_src.device_uid == node_dst.device_uid
+        return _make_key(node_src.tree_type, node_dst.tree_type, is_same_tree=is_same_tree)
+    else:
+        return _make_key(node_src.tree_type)
+
+
 LO = _make_key(TreeType.LOCAL_DISK)
 GD = _make_key(TreeType.GDRIVE)
-LO_LO = _make_key(TreeType.LOCAL_DISK, TreeType.LOCAL_DISK)
-GD_GD = _make_key(TreeType.GDRIVE, TreeType.GDRIVE)
 LO_GD = _make_key(TreeType.LOCAL_DISK, TreeType.GDRIVE)
 GD_LO = _make_key(TreeType.GDRIVE, TreeType.LOCAL_DISK)
+LO_same_LO = _make_key(TreeType.LOCAL_DISK, TreeType.LOCAL_DISK, is_same_tree=True)
+LO_different_LO = _make_key(TreeType.LOCAL_DISK, TreeType.LOCAL_DISK, is_same_tree=False)
+GD_same_GD = _make_key(TreeType.GDRIVE, TreeType.GDRIVE, is_same_tree=True)
+GD_different_GD = _make_key(TreeType.GDRIVE, TreeType.GDRIVE, is_same_tree=False)
 
 
 class CommandBuilder:
@@ -47,10 +58,10 @@ class CommandBuilder:
 
         if op.dst_node:
             # Src AND Dst:
-            tree_type_key = _make_key(op.src_node.node_identifier.tree_type, op.dst_node.node_identifier.tree_type)
+            tree_type_key = _make_key_from_node(op.src_node, op.dst_node)
         else:
             # Only Src:
-            tree_type_key = _make_key(op.src_node.node_identifier.tree_type)
+            tree_type_key = _make_key_from_node(op.src_node)
 
         tree_type_dict = self._build_dict.get(op.op_type)
         if not tree_type_dict:
@@ -63,37 +74,43 @@ class CommandBuilder:
         return build_func(uid, op)
 
 
+def _fail(change, key):
+    raise RuntimeError(f'No command for {key} & {change}')
+
+
 def _populate_build_dict():
     """Every command has an associated target node and a UserOp. Many commands also have an associated source node."""
-    return {UserOpType.MKDIR: {
-        GD: lambda uid, change: CreateGDriveFolderCommand(op=change, uid=uid),
-        LO: lambda uid, change: CreatLocalDirCommand(op=change, uid=uid)
-    }, UserOpType.CP: {
-        LO_LO: lambda uid, change: CopyFileLocallyCommand(uid, change, overwrite=False),
-
-        GD_GD: lambda uid, change: CopyFileGDriveCommand(uid, change),
-
-        LO_GD: lambda uid, change: UploadToGDriveCommand(uid, change, overwrite=False),
-
-        GD_LO: lambda uid, change: DownloadFromGDriveCommand(uid, change, overwrite=False)
-    }, UserOpType.MV: {
-        LO_LO: lambda uid, change: MoveFileLocallyCommand(uid, change),
-
-        GD_GD: lambda uid, change: MoveFileGDriveCommand(uid, change),
-
-        LO_GD: lambda uid, change: UploadToGDriveCommand(uid, change, overwrite=False),
-
-        GD_LO: lambda uid, change: DownloadFromGDriveCommand(uid, change, overwrite=False)
-    }, UserOpType.RM: {
-        LO: lambda uid, change: DeleteLocalNodeCommand(uid, change, to_trash=False),
-
-        GD: lambda uid, change: DeleteGDriveNodeCommand(uid, change, to_trash=False)
-    }, UserOpType.UP: {
-        LO_LO: lambda uid, change: CopyFileLocallyCommand(uid, change, overwrite=True),
-
-        GD_GD: lambda uid, change: CopyFileGDriveCommand(uid, change, overwrite=True),
-
-        LO_GD: lambda uid, change: UploadToGDriveCommand(uid, change, overwrite=True),
-
-        GD_LO: lambda uid, change: DownloadFromGDriveCommand(uid, change, overwrite=True),
-    }}
+    return {
+        UserOpType.MKDIR: {
+            GD: lambda uid, change: CreateGDriveFolderCommand(op=change, uid=uid),
+            LO: lambda uid, change: CreatLocalDirCommand(op=change, uid=uid)
+        },
+        UserOpType.RM: {
+            LO: lambda uid, change: DeleteLocalNodeCommand(uid, change, to_trash=False),
+            GD: lambda uid, change: DeleteGDriveNodeCommand(uid, change, to_trash=False)
+        },
+        UserOpType.CP: {
+            LO_same_LO: lambda uid, change: CopyFileLocallyCommand(uid, change, overwrite=False),
+            GD_same_GD: lambda uid, change: CopyFileGDriveCommand(uid, change),
+            LO_GD: lambda uid, change: UploadToGDriveCommand(uid, change, overwrite=False),
+            GD_LO: lambda uid, change: DownloadFromGDriveCommand(uid, change, overwrite=False),
+            LO_different_LO: lambda uid, change: _fail(change, LO_different_LO),
+            GD_different_GD: lambda uid, change: _fail(change, GD_different_GD),
+        },
+        UserOpType.MV: {
+            LO_same_LO: lambda uid, change: MoveFileLocallyCommand(uid, change),
+            GD_same_GD: lambda uid, change: MoveFileGDriveCommand(uid, change),
+            LO_GD: lambda uid, change: UploadToGDriveCommand(uid, change, overwrite=False),
+            GD_LO: lambda uid, change: DownloadFromGDriveCommand(uid, change, overwrite=False),
+            LO_different_LO: lambda uid, change: _fail(change, LO_different_LO),
+            GD_different_GD: lambda uid, change: _fail(change, GD_different_GD),
+        },
+        UserOpType.UP: {
+            LO_same_LO: lambda uid, change: CopyFileLocallyCommand(uid, change, overwrite=True),
+            GD_same_GD: lambda uid, change: CopyFileGDriveCommand(uid, change, overwrite=True),
+            LO_GD: lambda uid, change: UploadToGDriveCommand(uid, change, overwrite=True),
+            GD_LO: lambda uid, change: DownloadFromGDriveCommand(uid, change, overwrite=True),
+            LO_different_LO: lambda uid, change: _fail(change, LO_different_LO),
+            GD_different_GD: lambda uid, change: _fail(change, GD_different_GD),
+        }
+    }
